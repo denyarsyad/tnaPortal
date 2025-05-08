424f231df35c26c088b114999e2a9f377e8d0931
denyarsyad <deny.arsyad17@gmail.com>
2025-05-08 22:50:40 +0700

    no updates actually

--- application/views/login.php
+++ application/views/login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -9,7 +8,7 @@
 	<meta name="description" content="IT Helpdesk <?= $this->settings->info['perusahaan']; ?>">
 	<meta name="author" content="<?= $this->config->item('site_company'); ?>">
 	<title><?= $this->settings->info['aplikasi']; ?> <?= $this->settings->info['perusahaan']; ?></title>
-	
+
 	<!-- Custom fonts for this template-->
 	<link href="<?= base_url() ?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
